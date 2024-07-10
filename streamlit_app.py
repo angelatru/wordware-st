75757838b63ff885f31d095619af6e4c9272e666
Angela Truong <71371655+angelatru@users.noreply.github.com>
2024-07-10 05:23:53 +0000

    Starting to integrate Wordware with Streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,54 @@
+import streamlit as st
+import json
+import requests
 
+st.text_input("Enter something")
+
+
+def main():
+    prompt_id = "d27ebecc-6e11-44da-9057-9166a82af99c"
+    api_key = "ww-KyaGm2qynorDsCD1HhxmuRQbJZgiChX4IrENuuhJRTxCD6aYu6MaI1"
+
+    # Describe the prompt (shows just the inputs for now)
+    r1 = requests.get(f"https://app.wordware.ai/api/prompt/{prompt_id}/describe",
+                      headers={"Authorization": f"Bearer {api_key}"})
+    st.write(json.dumps(r1.json(), indent=4))
+
+    r = requests.post(f"https://app.wordware.ai/api/prompt/{prompt_id}/run",
+                      json={
+                        # I AM STUCK HERE, WHAT DO I DO THIS JSON
+                          "inputs":{
+
+                          }
+                      },
+                      headers={"Authorization": f"Bearer {api_key}"},
+                      stream=True
+                      )
+
+    # Ensure the request was successful
+    if r.status_code != 200:
+        st.write("Request failed with status code", r.status_code)
+        st.write(json.dumps(r.json(), indent=4))
+    else:
+        for line in r.iter_lines():
+            if line:
+                content = json.loads(line.decode('utf-8'))
+                value = content['value']
+                # We can print values as they're generated
+                if value['type'] == 'generation':
+                    if value['state'] == "start":
+                        st.write("\nNEW GENERATION -", value['label'])
+                    else:
+                        st.write("\nEND GENERATION -", value['label'])
+                elif value['type'] == "chunk":
+                    st.write(value['value'], end="")
+                elif value['type'] == "outputs":
+                    # Or we can read from the outputs at the end
+                    # Currently we include everything by ID and by label - this will likely change in future in a breaking
+                    # change but with ample warning
+                    st.write("\nFINAL OUTPUTS:")
+                    st.write(json.dumps(value, indent=4))
+
+
+if __name__ == '__main__':
+    main()
